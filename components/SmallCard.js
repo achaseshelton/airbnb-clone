2204aa4ec64f56c2b4c733a3968ae3f2b951cb10
Chase <austen.shelton@gmail.com>
2023-01-09 11:00:59 -0500

    adding small card component

---
+++ components/SmallCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Image from 'next/image';
+
+export class SmallCard extends Component {
+  render() {
+    return (
+    <div className='flex items-center m-2 mt-5 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 
+    transition transform duration-200 ease-out'>      
+      <div className='relative h-16 w-16'>
+        <Image src={img} layout="fill" className="rounded-lg" />       
+      </div>
+      <div>
+        <h2>{location}</h2>
+        <h3 className='text-gray-500'>{distance}</h3>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default SmallCard
